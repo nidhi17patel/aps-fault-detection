bed9f255d640ff3b77db314d0aaab9dcf3f98a57
nidhi17patel <nidhi17patel@gmail.com>
2022-12-02 15:21:00 +0530

    updated the requirements.txt file second time

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import find_packages,setup
 
+from typing import List
 
-def get_requirements():
-    pass
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
 
 setup(
     name="sensor",
-    version="0.0.2",
+    version="0.0.1",
     author="nidhi17patel",
     author_email="nidhi17patel@gmail.com",
     packages = find_packages(),
