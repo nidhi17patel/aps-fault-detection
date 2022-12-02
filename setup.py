88cc7ca0a1d1bc81190cf8e4f253b17112fc941d
nidhi17patel <nidhi17patel@gmail.com>
2022-12-02 14:57:16 +0530

    updated the requirements.txt file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.2",
+    author="nidhi",
+    author_email="nidhi17patel@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
