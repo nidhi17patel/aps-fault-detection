16e5ebb9c7e0d3827f490e9c2288f0dd78bfa8d7
nidhi17patel <nidhi17patel@gmail.com>
2022-12-12 12:54:21 +0530

    updated all

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.2",
-    author="nidhi17patel",
-    author_email="nidhi17patel@gmail.com",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
